440331948c031aeb6881cacfd783cfc272a2f15e
jabhay2003 <163547129+jabhay2003@users.noreply.github.com>
2025-12-17 13:10:31 +0530

    Refactor command processing and update initialization message

--- main.py
+++ main.py
@@ -10,18 +10,30 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def processCommand(c):
+     if "open google" in c.lower():
+         speak("Opening Google")
+         webbrowser.open("https://www.google.com")
+
 if __name__ == "__main__":
-    speak("Initializing AskMe...")
+    speak("Initializing Jarvis...")
     while True:
         # Listen for the wake word "AskMe"
         # obtain audio from the microphone
         r = sr.Recognizer()
-        with sr.Microphone() as source:
-                print("Listening...")
-                audio = r.listen(source)
         print("recognizing...")
         try:
-            command = r.recognize_google(audio)
-            print(command)
+            with sr.Microphone() as source:
+                    print("Listening...")
+                    audio = r.listen(source,timeout=2,phrase_time_limit=2)
+            word = r.recognize_google(audio)
+            if word.lower() == "jarvis":
+                 speak("How can I help you?")
+                 # Listen for command
+                 with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    command = r.recognize_google(audio)
+                    processCommand(command)
+        
         except sr.UnknownValueError:
              print("Could not understand audio")
